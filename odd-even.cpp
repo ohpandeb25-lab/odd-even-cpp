74939fdb58432aa9c771a5afa7015faf104a70b3
shreyansdeshp <sndeshpande_b25@it.vjti.ac.in>
2026-08-14 07:19:21 +0530

    CHANGES MADE IN MAIN BRANCH

--- odd-even.cpp
+++ odd-even.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n; #This variable is used to store the number input by the user. It is declared as an integer type since we are dealing with whole numbers. 
 
-    cout << "enter a number: ";
+    cout << "Enter a number: "; #Changed the case sensitivity of the output to make it more user-friendly and readable.
     cin >> n;
 
-    if(n % 2 == 0)
+    if(n % 2 == 0) #This is done to check if the number is even or odd as even numbers are divisible by 2 and odd numbers are not.
         cout << "even";
     else
         cout << "odd";
